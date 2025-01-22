acb0b06f578fff99f78d60deae92aed7dcb852a9
liShafor <elizavetashaforostovaxr@gmail.com>
2025-01-22 20:18:34 +0300

    Внесены изменения функционала: изменение координат персонажа

--- app/characters/model.py
+++ app/characters/model.py
@@ -6,10 +6,23 @@ class Character:
     def __init__(self, character_name: str, info: str, character_id: int = None):
         self.character_name = character_name
         self.info = info
+        self.coors = ()
+        self.speed = 10
         self.info_from_json = json.loads(info)
         if character_id:
             self.character_id = character_id
         self.active = True
+
+    def load_image(self):
+        pass
+
+    def move(self, word: str):
+        match_word = {'left': lambda x: (self.coors[0] - self.speed, self.coors[1]),
+                      'right': lambda x: (self.coors[0] + self.speed, self.coors[1]),
+                      'dowm': lambda x: (self.coors[0], self.coors[1] - self.speed),
+                      'up': lambda x: (self.coors[0], self.coors[1] + self.speed)
+                      }
+        self.coors = match_word[word]
 
     def get_info(self):
         return self.info
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import pygame
+
 from database import Connection, create_database
+from app.characters.model import Character
 
 
 create_database()
-pygame.init()
+if __name__ == '__main__':
+    pygame.init()
+    running = True
+    character = Character()
+    if running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    character.move('up')
+                if event.key == pygame.K_a:
+                    character.move('left')
+                if event.key == pygame.K_s:
+                    character.move('down')
+                if event.key == pygame.K_d:
+                    character.move('right')
