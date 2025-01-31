7f08bda120c8831137d156900e33c724b3128c0a
liShafor <elizavetashaforostovaxr@gmail.com>
2025-02-01 00:19:09 +0300

    Отображение карты - незакончено

--- app/map/model.py
+++ app/map/model.py
@@ -7,37 +7,36 @@ from app.characters.model import Character
 
 class Map:
     def __init__(self, filename, window_size):
-        self.map = pytmx.TiledMap(os.path.join('map', filename))
-
+        self.map = pytmx.TiledMap(os.path.join('app\map', filename))
         self.width = self.map.width
         self.height = self.map.height
         self.tile_size = self.map.tilewidth
         self.window_size = window_size
         self.collision = self.map.get_layer_by_name('walls')
-        self.CAMERA = self.map.get_object_by_name("Player")
+        # self.CAMERA = self.map.get_object_by_name("Player")
         self.tiles = []
         for x, y, tile in self.collision.tiles():
             if tile:
                  self.tiles.append(pygame.Rect([(x * self.tile_size), (y * self.tile_size),
                                                 self.tile_size, self.tile_size]))
 
-    def render(self, screen, player):  # <- надо будет поменять
-        for layer in self.map.layers:
-            if isinstance(layer, pytmx.TiledTileLayer):
-                for x, y, tile in layer.tiles():
-                   if tile:
-                        screen.blit(tile, [(x * self.tile_size) - self.CAMERA.x + (self.window_size[0] / 2),
-                                           (y * self.tile_size) - self.CAMERA.y + (self.window_size[1] / 2)])
+    def render(self, screen, player=None):  # <- надо будет поменять
+        try:
+            for layer in self.map.layers:
+                if isinstance(layer, pytmx.TiledTileLayer):
+                    for x, y, tile in layer.tiles():
+                        screen.blit(pygame.image.load(f'app/view/images/{list(tile)[0].split("/")[-1]}'),
+                                    [x * self.tile_size,y * self.tile_size])
 
-            elif isinstance(layer, pytmx.TiledObjectGroup):
-                for object in layer:
-                    if object.type == 'Player':
-                        screen.blit(player,
-                                    [object.x - self.CAMERA.x + (self.window_size[0] / 2),
-                                     object.y - self.CAMERA.y + (self.window_size[1] / 2)])
-
-    def update(self):
-        pass
+                elif isinstance(layer, pytmx.TiledObjectGroup):
+                    for object in layer:
+                        if object.type == 'Player':
+                            screen.blit(player,
+                                        [object.x + (self.window_size[0] / 2),
+                                         object.y + (self.window_size[1] / 2)])
+                pygame.display.update()
+        except Exception as e:
+            print(e)
 
     def checktiles(self, player_rect):
         check = False
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from database import Connection, create_database
 from app.characters.model import Character
+from app.map.model import Map
 
 
 def start_screen(screen, w, h):
@@ -16,25 +17,26 @@ create_database()
 if __name__ == '__main__':
     pygame.init()
 
-    size = width, height = 320, 240
+    size = width, height = 400, 400
     screen = pygame.display.set_mode(size)
     running = True
-
-    character = Character()
+    map_game = Map('map1.tmx', size)
+    # character = Character()
     if running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    character.move('up')
-                if event.key == pygame.K_a:
-                    character.move('left')
-                if event.key == pygame.K_s:
-                    character.move('down')
-                if event.key == pygame.K_d:
-                    character.move('right')
-
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_w:
+            #         character.move('up')
+            #     if event.key == pygame.K_a:
+            #         character.move('left')
+            #     if event.key == pygame.K_s:
+            #         character.move('down')
+            #     if event.key == pygame.K_d:
+            #         character.move('right')
+        map_game.render(screen)
+        pygame.display.update()
 # получение рейтинг борда
 
 
