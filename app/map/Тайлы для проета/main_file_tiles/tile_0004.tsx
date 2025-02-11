8cffd598243f0f106af0b9345a3e51ea7029b6ca
Batonchi <gkok.gkok@yandex.ru>
2025-02-11 11:45:47 +0300

    Исправлены ошибки. Переопределена функция создания еарты.

---
+++ app/map/Тайлы для проета/main_file_tiles/tile_0004.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="tile_0004" tilewidth="32" tileheight="32" tilecount="275" columns="11">
+ <image source="C:\Python Projects\gameProject\app\view\images\tile_0004.png" width="382" height="805"/>
+</tileset>
