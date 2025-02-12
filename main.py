3204c8aaeeb7be7857e4dca6fde9c7c61856caf2
liShafor <elizavetashaforostovaxr@gmail.com>
2025-02-12 22:14:52 +0300

    Изменения

--- main.py
+++ main.py
@@ -74,21 +74,114 @@ class Game:
         pass
 
 
+class Menu:
+    def __init__(self):
+        pass
+
+    def start_game(self):
+        return False
+
+    def show_about_the_game(self, screen):
+        screen.fill((0, 0, 0))
+        pygame.display.set_caption('Об игре')
+        button_back = Button(screen, 150, 300, 200, 80, text='Назад',
+                             fontSize=30, margin=20,
+                             inactiveColour=(250, 250, 250),
+                             pressedColour=(0, 255, 0), radius=0,
+                             onClick=None)
+        show_menu = True
+        while show_menu:
+            events = pygame.event.get()
+            for e in events:
+                if e.type == pygame.QUIT:
+                    show_menu = False
+                    quit()
+                if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                    if (150 < e.pos[0] and e.pos[0] < 150 + 200 and 400 < e.pos[1] and e.pos[1] < 400 + 80):
+                        show_menu = False
+            button_back.listen(events)
+            button_back.draw()
+            pygame.display.update()
+        return
+
+    def show_trainig_screen(self, screen):
+        screen.fill((0, 0, 0))
+        pygame.display.set_caption('Обучение')
+        button_back = Button(screen, 300, 700, 200, 80, text='Назад',
+                             fontSize=30, margin=20,
+                             inactiveColour=(250, 250, 250),
+                             pressedColour=(0, 255, 0), radius=0,
+                             onClick=None)
+        show_menu = True
+        while show_menu:
+            events = pygame.event.get()
+            for e in events:
+                if e.type == pygame.QUIT:
+                    show_menu = False
+                    quit()
+                if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                    if (500 < e.pos[0] and e.pos[0] < 500 + 200 and 400 < e.pos[1] and e.pos[1] < 400 + 80):
+                        show_menu = False
+            button_back.listen(events)
+            button_back.draw()
+            pygame.display.update()
+        return
+
+    def main_menu(self, screen):
+        screen.fill((0, 0, 0))
+        show_main_menu = True
+        pygame.display.set_caption('Меню')
+
+        button_play = Button(screen, 500, 150, 200, 100, text='Новая игра',
+                             fontSize=30, margin=20,
+                             inactiveColour=(250, 250, 250),
+                             pressedColour=(0, 255, 0), radius=0,
+                             onClick=None)
+        button_training = Button(screen, 500, 260, 200, 100, text='Обучение',
+                                 fontSize=30, margin=20,
+                                 inactiveColour=(250, 250, 250),
+                                 pressedColour=(0, 255, 0), radius=0,
+                                 onClick=None)
+        button_about_the_game = Button(screen, 500, 370, 200, 100, text='Об игре',
+                                       fontSize=30, margin=20,
+                                       inactiveColour=(250, 250, 250),
+                                       pressedColour=(0, 255, 0), radius=0,
+                                       onClick=None)
+        button_exit = Button(screen, 500, 480, 200, 100, text='Выход',
+                             fontSize=30, margin=20,
+                             inactiveColour=(250, 250, 250),
+                             pressedColour=(0, 255, 0), radius=0,
+                             onClick=None)
+        while show_main_menu:
+            events = pygame.event.get()
+            for e in events:
+                if e.type == pygame.QUIT:
+                    show_main_menu = False
+                    pygame.quit()
+                if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                    if (500 < e.pos[0] and e.pos[0] < 700 and 150 < e.pos[1] and e.pos[1] < 250):
+                        show_main_menu = self.start_game()
+                        if not show_main_menu:
+                            break
+                    if (500 < e.pos[0] and e.pos[0] < 700 and 260 < e.pos[1] and e.pos[1] < 360):
+                        self.show_trainig_screen(pygame.display.set_mode((800, 800)))
+                    if (500 < e.pos[0] and e.pos[0] < 700 and 370 < e.pos[1] and e.pos[1] < 440):
+                        self.show_about_the_game(pygame.display.set_mode((800, 800)))
+                    if (500 < e.pos[0] and e.pos[0] < 700 and 480 < e.pos[1] and e.pos[1] < 580):
+                        quit()
+            bg_menu = pygame.image.load('app/view/images/for_main_menu.jpg')
+            screen.blit(bg_menu, (-25, -50))
+            button_play.listen(events)
+            button_play.draw()
+            button_training.listen(events)
+            button_training.draw()
+            button_about_the_game.listen(events)
+            button_about_the_game.draw()
+            button_exit.listen(events)
+            button_exit.draw()
+
+            pygame.display.update()
+
+
 if __name__ == '__main__':
     game = Game()
-
-
-
-
-# if event.key == pygame.K_w:
-#     coors = character.move('up')
-#     map_game.render(screen, coors)
-# elif event.key == pygame.K_a:
-#     coors = character.move('left')
-#     map_game.render(screen, coors)
-# elif event.key == pygame.K_s:
-#     coors = character.move('down')
-#     map_game.render(screen, coors)
-# elif event.key == pygame.K_d:
-#     coors = character.move('right')
-#     map_game.render(screen, coors)
